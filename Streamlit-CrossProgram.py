a488fa03849ec670a5a61afd331e580a3fd813a0
shansen5 <steven.e.hansen@gmail.com>
2023-03-09 21:42:53 -0800

    Trying out some basics

--- Streamlit-CrossProgram.py
+++ Streamlit-CrossProgram.py
@@ -1,1 +1,41 @@
-print('Hello EDGI!')
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+from ECHO_modules.get_data import get_echo_data
+
+st.title('Cross Program Data')
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+st.line_chart(dataframe)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+x = st.slider('x')  # 👈 this is a widget
+str = "The square root of {} is".format(x)
+st.write(str, math.sqrt(x))
